eaedb135631bcb0b0f86f19d2aaa38ba043c05b8
levinh0612 <72688487+levinh0612@users.noreply.github.com>
2021-12-29 08:58:15 +0700

    phien ban hoan thien v3.0

--- admin/pdo.php
+++ admin/pdo.php
@@ -1,3 +1,3 @@
 <?php
-$objPDO = new PDO('mysql:host=localhost:3307;dbname=laptopstore'  ,'root' , '' );
+$objPDO = new PDO('mysql:host=localhost;dbname=laptopstore'  ,'root' , '' );
 $objPDO->query('set names utf8');
--- models/Db.php
+++ models/Db.php
@@ -1,9 +1,10 @@
 <?php
+
 class Db{
     protected static $pdo;
     function __construct()
     {
-        self::$pdo = new PDO('mysql:host='. HOST .';dbname='. DB, USER , PW);
+        self::$pdo = new PDO('mysql:host=localhost;dbname=laptopstore'  ,'root' , '' );
         self::$pdo->query('set names utf8');
     }
 
